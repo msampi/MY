8a2be56b3ba0bee2559a42f76c54989fe11d3062
matiassampietro <matias2118121181>
2021-04-23 15:39:18 -0300

    missing styles

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -52,7 +52,7 @@
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b>MY</b></span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><img class="admin-logo" src="{{ URL::asset('img/Logo500.png') }}"> <b>Magyates</b></span>
+          <span class="logo-lg"><img class="admin-logo" src="{{ URL::asset('img/logo500.png') }}"> <b>Magyates</b></span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
